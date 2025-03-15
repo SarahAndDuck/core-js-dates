e8da2325027942b5d4f2edc18f7978595754e908
SarahAndDuck <ratkevirina@gmail.com>
2025-03-15 21:24:27 +0700

    feat: add dateToTimestamp, getTime, getDayName solutions

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -77,8 +77,20 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  let result = date;
+  if (date.getUTCDay() > 5) {
+    result = new Date(
+      Date.parse(date) + date.getUTCDay() * 24 * 60 * 60 * 1000
+    );
+  } else if (date.getUTCDay() === 5) {
+    result = new Date(Date.parse(date) + 7 * 24 * 60 * 60 * 1000);
+  } else if (date.getUTCDay() <= 5) {
+    result = new Date(
+      Date.parse(date) + (5 - date.getUTCDay()) * 24 * 60 * 60 * 1000
+    );
+  }
+  return result;
 }
 
 /**
